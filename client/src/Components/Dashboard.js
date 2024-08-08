7e4b70a01713d6bf244dae2d8969c8feab71f008
Taimoor Habib <taimoorhabib948@gmail.com>
2024-08-08 11:41:25 +0500

    Update Dashboard.js

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -9,7 +9,7 @@ const Dashboard = () => {
 
   const getUser = async () => {
     try {
-        const response = await axios.get("http://localhost:6005/login/sucesss", { withCredentials: true });
+        const response = await axios.get("https://deploy-mern-oauth-proj-1-ui.vercel.app/login/sucesss", { withCredentials: true });
 
         console.log("response",response)
     } catch (error) {
